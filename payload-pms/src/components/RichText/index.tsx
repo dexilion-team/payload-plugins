910386e7f1ddcf2ae98708556e4d1d2d75df84f8
Mark Tolmacs <mark@lazycat.hu>
2026-02-08 22:18:48 +0100

    fix: Clean up RichText
    Signed-off-by: Mark Tolmacs <mark@lazycat.hu>

--- payload-pms/src/components/RichText/index.tsx
+++ payload-pms/src/components/RichText/index.tsx
@@ -2,56 +2,9 @@
 
 import type { SerializedEditorState } from "@payloadcms/richtext-lexical/lexical";
 import { convertLexicalToHTMLAsync } from "@payloadcms/richtext-lexical/html-async";
-import {
-  getRestPopulateFn,
-  NodeFormat,
-} from "@payloadcms/richtext-lexical/client";
+import { getRestPopulateFn } from "@payloadcms/richtext-lexical/client";
 
 import React, { useEffect, useState } from "react";
-
-const customConverter = async ({
-  node,
-  providedStyleTag,
-}: {
-  node: any;
-  providedStyleTag: any;
-}) => {
-  const sub = node.children.map((child: any) => {
-    let text = child.text;
-
-    if (!text) {
-      return "";
-    }
-
-    if (child.format & NodeFormat.IS_BOLD) {
-      text = `<strong>${text}</strong>`;
-    }
-    if (child.format & NodeFormat.IS_ITALIC) {
-      text = `<em>${text}</em>`;
-    }
-    if (child.format & NodeFormat.IS_STRIKETHROUGH) {
-      text = `<span style="text-decoration: line-through">${text}</span>`;
-    }
-    if (child.format & NodeFormat.IS_UNDERLINE) {
-      text = `<span style="text-decoration: underline">${text}</span>`;
-    }
-    if (child.format & NodeFormat.IS_CODE) {
-      text = `<code>${text}</code>`;
-    }
-    if (child.format & NodeFormat.IS_SUBSCRIPT) {
-      text = `<sub>${text}</sub>`;
-    }
-    if (child.format & NodeFormat.IS_SUPERSCRIPT) {
-      text = `<sup>${text}</sup>`;
-    }
-    if (child.style) {
-      text = `<span style="${child.style}">${text}</span>`;
-    }
-
-    return text;
-  });
-  return `<${node.tag}${providedStyleTag}>${sub.join("")}</${node.tag}>`;
-};
 
 export const RichText = ({ content }: { content: SerializedEditorState }) => {
   const [html, setHTML] = useState<null | string>(null);
@@ -65,8 +18,6 @@ export const RichText = ({ content }: { content: SerializedEditorState }) => {
         converters: ({ defaultConverters }) => {
           return {
             ...defaultConverters,
-            // "custom-heading": customConverter,
-            // "custom-paragraph": customConverter,
             vimeo: async ({ node }) =>
               `<iframe
                 src="https://player.vimeo.com/video/${node.id}"
