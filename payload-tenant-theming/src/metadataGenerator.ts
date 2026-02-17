413aa931d1eec681c28ce96152f3219d52711007
Mark Tolmacs <mark@dexilion.com>
2026-02-17 14:28:19 +0000

    fix(metadata): Add base from tenant
    Signed-off-by: Mark Tolmacs <mark@dexilion.com>

--- payload-tenant-theming/src/metadataGenerator.ts
+++ payload-tenant-theming/src/metadataGenerator.ts
@@ -6,7 +6,7 @@ import { SanitizedConfig } from "payload";
 export const metadataGenerator =
   (
     payloadConfig: Promise<SanitizedConfig>,
-    options?: { tenantsSlug?: string },
+    options?: { tenantsKey?: string; domainTenantKey?: string },
   ) =>
   async ({ params }: { params: any; searchParams: any }): Promise<Metadata> => {
     const { segments } = (await params) as { segments?: string[] };
@@ -20,25 +20,30 @@ export const metadataGenerator =
       return {};
     }
 
-    const tenantsSlug = options?.tenantsSlug || "tenants";
-    const tenantName = page[tenantsSlug]?.name || "";
+    const tenantsKey = options?.tenantsKey || "tenant";
+    const domainKey = options?.domainTenantKey || "domain";
+    const tenantName = page[tenantsKey]?.[domainKey];
     const path = page.path ?? `/${page.id}`;
-    const logo = page.meta?.image?.url ?? page[tenantsSlug]?.logo?.url;
+    const logo = page.meta?.image?.url ?? page[tenantsKey]?.logo?.url;
     const title =
       (page.meta.title ||
         recursivelySearchForDataByName<string>(page, "title", ["parent"])) ??
       "";
 
     return {
+      metadataBase: tenantName ? new URL(`https://${tenantName}`) : undefined,
       title: title,
+      alternates: {
+        canonical: path,
+      },
       openGraph: {
         title: title,
-        url: `https://${tenantName}/${path}`,
+        url: path,
         images: logo ? [logo] : undefined,
       },
       twitter: {
-        title: page.title,
-        site: `https://${tenantName}/${path}`,
+        title: title,
+        site: path,
         images: logo ? [logo] : undefined,
       },
     };
