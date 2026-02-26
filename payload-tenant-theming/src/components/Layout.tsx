6a9260dd4d24919d5febaf62e4398c451104b0c8
Mark Tolmacs <mark@dexilion.com>
2026-02-26 14:05:36 +0000

    feat: Theme loader test
    Signed-off-by: Mark Tolmacs <mark@dexilion.com>

--- payload-tenant-theming/src/components/Layout.tsx
+++ payload-tenant-theming/src/components/Layout.tsx
@@ -9,6 +9,7 @@ export const Layout = async ({
   children,
 }: PropsWithChildren<{ payloadConfig: Promise<SanitizedConfig> }>) => {
   let themeHref: string | null = null;
+  let themeCss: string | null = null;
 
   try {
     const tenantName = await getTenantName();
@@ -17,16 +18,19 @@ export const Layout = async ({
         payloadConfig,
         tenantName,
       });
-      themeHref =
-        process.env.NODE_ENV === "production"
-          ? `/${encodeURIComponent(theme.name)}/global.css`
-          : `/api/theme.css?${encodeURIComponent(theme.name)}`;
+      themeHref = `/themes/${encodeURIComponent(theme.name)}/theme.generated.css`;
+      themeCss = `/api/theme.css?${encodeURIComponent(theme.name)}`;
+      // themeHref =
+      //   process.env.NODE_ENV === "production"
+      //     ? `/${encodeURIComponent(theme.name)}/global.css`
+      //     : `/api/theme.css?${encodeURIComponent(theme.name)}`;
     }
   } catch {}
 
   return (
     <html>
       <head>
+        {themeCss ? <link rel="stylesheet" href={themeCss} /> : null}
         {themeHref ? <link rel="stylesheet" href={themeHref} /> : null}
       </head>
       <body>{children}</body>
