080638fbaee36eeb8171e9649260d97bc45abab2
Mark Tolmacs <mark@lazycat.hu>
2026-04-20 08:49:11 +0200

    chore: Clone template
    Signed-off-by: Mark Tolmacs <mark@lazycat.hu>

---
+++ plugin/dev/payload.config.ts
@@ -0,0 +1,86 @@
+import { mongooseAdapter } from "@payloadcms/db-mongodb";
+import { lexicalEditor } from "@payloadcms/richtext-lexical";
+import { MongoMemoryReplSet } from "mongodb-memory-server";
+import path from "path";
+import { buildConfig } from "payload";
+import { myPlugin } from "../src";
+import sharp from "sharp";
+import { fileURLToPath } from "url";
+
+import { devUser } from "./helpers/credentials";
+import { testEmailAdapter } from "./helpers/testEmailAdapter";
+import { seed } from "./seed";
+
+const filename = fileURLToPath(import.meta.url);
+const dirname = path.dirname(filename);
+
+if (!process.env.ROOT_DIR) {
+  process.env.ROOT_DIR = dirname;
+}
+
+const buildConfigWithMemoryDB = async () => {
+  const memoryDB = await MongoMemoryReplSet.create({
+    replSet: {
+      count: 3,
+      dbName: "payloadmemory",
+    },
+  });
+
+  process.env.DATABASE_URL = `${memoryDB.getUri()}&retryWrites=true`;
+
+  return buildConfig({
+    admin: {
+      importMap: {
+        autoGenerate: true,
+        baseDir: path.resolve(dirname),
+      },
+      autoLogin: {
+        email: devUser.email,
+        password: devUser.password,
+      },
+    },
+    collections: [
+      {
+        slug: "posts",
+        fields: [],
+      },
+      {
+        slug: "media",
+        fields: [],
+        upload: {
+          staticDir: path.resolve(dirname, "media"),
+        },
+      },
+      {
+        slug: "users",
+        auth: true,
+        admin: {
+          useAsTitle: "email",
+          defaultColumns: ["email", "updatedAt", "createdAt"],
+        },
+        fields: [],
+      },
+    ],
+    db: mongooseAdapter({
+      ensureIndexes: true,
+      url: process.env.DATABASE_URL || "",
+    }),
+    editor: lexicalEditor(),
+    email: testEmailAdapter,
+    onInit: async (payload) => {
+      await seed(payload);
+    },
+    plugins: [
+      myPlugin({
+        // Configure your plugin options here
+      }),
+    ],
+    secret: process.env.PAYLOAD_SECRET || "test-secret_key",
+    sharp,
+    typescript: {
+      outputFile: path.resolve(dirname, "payload-types.ts"),
+    },
+  });
+};
+
+export default buildConfigWithMemoryDB();
