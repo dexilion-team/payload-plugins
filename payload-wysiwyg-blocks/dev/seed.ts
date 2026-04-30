88c3c0504a27af1070741a5725ab00688d8beb9c
Mark Tolmacs <mark@dexilion.com>
2026-04-30 07:40:33 +0000

    chore: Dev env
    Signed-off-by: Mark Tolmacs <mark@dexilion.com>

--- payload-wysiwyg-blocks/dev/seed.ts
+++ payload-wysiwyg-blocks/dev/seed.ts
@@ -19,13 +19,32 @@ export const seed = async (payload: Payload) => {
     });
   }
 
-  console.log("   ");
-  console.log("+-----------------------------------------------------");
-  console.log("|   ");
-  console.log("|   Log into the admin dashboard with: ");
-  console.log(`|      Email:    ${devUser.email}`);
-  console.log(`|      Password: ${devUser.password}`);
-  console.log("|   ");
-  console.log("+-----------------------------------------------------");
-  console.log("   ");
+  const { totalDocs: widgetCount } = await payload.count({
+    collection: "widgets",
+  });
+
+  if (!widgetCount) {
+    await payload.create({
+      collection: "widgets",
+      data: {
+        name: "basic",
+        widget:
+          `<>\n` +
+          `  <Text name="title" label="Title" required />\n` +
+          `  <RichText name="body" label="Body" />\n` +
+          `  <Relationship name="image" label="Image" relationTo="media" />\n` +
+          `</>`,
+      },
+    });
+  }
+
+  payload.logger.info("   ");
+  payload.logger.info("+-----------------------------------------------------");
+  payload.logger.info("|   ");
+  payload.logger.info("|   Log into the admin dashboard with: ");
+  payload.logger.info(`|      Email:    ${devUser.email}`);
+  payload.logger.info(`|      Password: ${devUser.password}`);
+  payload.logger.info("|   ");
+  payload.logger.info("+-----------------------------------------------------");
+  payload.logger.info("   ");
 };
